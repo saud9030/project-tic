92dc1abd348cc2c74593146f478c5b89e1816f1c
saud9030 <saud.amakn@gmail.com>
2019-01-27 15:38:50 +0300

    day - finished the logic part(first version)
    * created the main files
    * bulid the base layout to test the game
    * finished with the logic part (changes might occure on the logic part if needed)
    * game works fine
    
    *game will return who won or a tie, and will refresh the game automatically

--- main.js
+++ main.js
@@ -6,3 +6,89 @@
 
 //use array to collect data
 
+
+// -----------------------
+var user = "user2"
+var user1 = 0;
+var user2 = 0;
+var tie=0;
+
+function testing(event){
+    
+    var turn = event.target;
+   
+    boxTaken(event);
+   
+    checkWinner(event);
+
+}
+// to change between users at every click and to insert input
+function userChange(){
+    var turn = event.target;
+
+    if (user === "user1"){
+        user = "user2";
+        user2 += 1;
+        $(turn).text("X")
+    }
+    else{
+        user = "user1";
+        user1 +=1;
+        $(turn).text("O")
+    }
+
+}
+// to check if the box is taken, and if it is then do not allow user input , otherwise go ahead
+function boxTaken() {
+    var turn = event.target;
+    if (turn.textContent === "X" || turn.textContent === "O"){
+        alert("error")
+    }
+    else{
+        $(turn).text("X")
+        userChange(event);   
+    }
+
+}
+// to check if someone has won the game 
+function checkWinner(){
+    var turn = event.target;
+    var boxIndex = $(".col").toArray();
+    tie += 1;
+    var win;
+    if (user === "user2"){
+        win = "X";
+    }
+    else{
+        win = "O";
+    }
+    if (
+        (boxIndex[0].textContent === win && boxIndex[1].textContent === win && boxIndex[2].textContent === win) 
+        ||
+        (boxIndex[3].textContent === win && boxIndex[4].textContent === win && boxIndex[5].textContent === win) 
+        ||
+        (boxIndex[6].textContent === win && boxIndex[7].textContent === win && boxIndex[8].textContent === win) 
+        ||
+        (boxIndex[0].textContent === win && boxIndex[4].textContent === win && boxIndex[8].textContent === win) 
+        ||
+        (boxIndex[0].textContent === win && boxIndex[3].textContent === win && boxIndex[6].textContent === win) 
+        ||
+        (boxIndex[1].textContent === win && boxIndex[4].textContent === win && boxIndex[7].textContent === win) 
+        ||
+        (boxIndex[2].textContent === win && boxIndex[5].textContent === win && boxIndex[8].textContent === win) 
+        ||
+        (boxIndex[2].textContent === win && boxIndex[4].textContent === win && boxIndex[6].textContent === win) 
+            ){
+        alert(` ${user} just won`)
+        location.reload(true);
+            }else if (tie === 9){
+                alert(`it's a tie`)
+                location.reload(true);
+
+            }
+    
+}
+
+
+$(".col").on("click",testing)
+
