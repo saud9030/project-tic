dd504a55efd3924673e4cd8146a309637454eabd
saud9030 <saud.amakn@gmail.com>
2019-01-27 09:18:28 +0300

    tic-tac-toe

---
+++ main.js
@@ -0,0 +1,8 @@
+// empty boxes to start to play with 
+// 8 cases of winning/losing , and everthing else will be tie <<< using if condintions/loops
+// create two users , making the game rotating between them after the first chooses
+// when the game ends a page will pop congrating the user
+//if the game ended it will restart itself again 
+
+//use array to collect data
+
